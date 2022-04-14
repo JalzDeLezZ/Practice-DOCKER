d60b00f243d00bda4521e07bc49a0527d6a1edb6
JalzDeLezZ <j4mes.delez@gmail.com>
2022-04-14 01:06:36 -0500

    practice 60%

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>sss</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {`${process.env.REACT_APP_NAME}`}
         </p>
         <a
           className="App-link"
